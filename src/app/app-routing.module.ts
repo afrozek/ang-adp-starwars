778ef3d1cfcbaf2dd7645feed336bfef21660a43
Afroze Khan <a.froze.ak@gmail.com>
2019-07-04 10:35:49 -0400

    setup nested routes skeleton for characters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CharactersComponent } from './characters/characters.component';
+import { CharacterDetailComponent } from './characters/character-detail/character-detail.component';
+import { CharactersHomeComponent } from './characters/characters-home/characters-home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutComponent},
+  { path: 'characters', component: CharactersComponent, children: [
+    { path: '', component: CharactersHomeComponent },
+    { path: ':id/detail', component: CharacterDetailComponent }
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
+import { AboutComponent } from './about/about.component';
+import { CharactersComponent } from './characters/characters.component';
+import { CharacterDetailComponent } from './characters/character-detail/character-detail.component';
+import { CharactersHomeComponent } from './characters/characters-home/characters-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
+    MainNavComponent,
+    AboutComponent,
+    CharactersComponent,
+    CharacterDetailComponent,
+    CharactersHomeComponent
   ],
   imports: [
     BrowserModule,
