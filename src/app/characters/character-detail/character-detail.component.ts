83f166786958618eec2f119439e820ae8d06c7c8
Afroze Khan <a.froze.ak@gmail.com>
2019-07-05 18:10:53 -0400

    fetching character detail and movie detail using forkjoin and displaying json on view

--- src/app/characters/character-detail/character-detail.component.ts
+++ src/app/characters/character-detail/character-detail.component.ts
@@ -8,6 +8,10 @@ import { CharactersService } from '../characters.service';
   styleUrls: ['./character-detail.component.scss']
 })
 export class CharacterDetailComponent implements OnInit {
+  test = 'test';
+  characterDetails: object;
+  characterMoviesDetails: Array<object>;
+
 
   constructor(private charactersService: CharactersService) { }
 
@@ -18,11 +22,41 @@ export class CharacterDetailComponent implements OnInit {
   private fetchCharacterDetail(id: number) {
     this.charactersService.getCharacterDetail(1)
     .subscribe(detail => {
-      console.log(detail);
+      console.log(detail.films);
+      this.characterDetails = detail;
+      this.fetchCharacterMoviesDetails(detail.films)
     }, error => {
       alert(error);
       console.log(error);
     })
   }
 
-}
+  // private fetchCharacterMoviesDetails() {
+  //   this.charactersService.getAllMoviesDetails()
+  //   .subscribe(([res1, res2, res3]) => {
+  //     console.log("res: ", res1)
+  //     console.log("res: ",  res2)
+  //     console.log("res: ",  res3)
+  //   },
+  //   error => {      
+  //     console.log("error: ", error);
+  //   })
+
+  // }
+
+  private fetchCharacterMoviesDetails(films: Array<string>) {
+    this.charactersService.getAllMoviesDetails(films)
+    .subscribe((res) => {
+      console.log("res: ", res);
+
+      this.characterMoviesDetails = res;
+
+    })
+
+  }
+
+  }
+
+  
+
+
--- src/app/characters/characters.service.ts
+++ src/app/characters/characters.service.ts
@@ -1,16 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { forkJoin, of, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharactersService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+    
+  }
 
   // get single character detail by id
   getCharacterDetail(id: Number) {
     return this.http.get(`https://swapi.co/api/people/${id}/`);
   }
+
+
+  getAllMoviesDetails(films: Array<string>) {
+
+    console.log(films)
+
+    let filmsObservablesList = films.map((film)=> {
+      return this.http.get(film)
+    })
+
+    console.log(filmsObservablesList)
+
+    return forkJoin(filmsObservablesList)
+
+ 
+  }
+
+
+  
 
 }
