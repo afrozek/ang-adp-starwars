3f760791cb19eaca38b259cea605d05b126f5bc9
Afroze Khan <a.froze.ak@gmail.com>
2019-07-04 20:22:47 -0400

    disabled view encapsulation for parent character component and added proper styling

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.component.html',
-  styleUrls: ['./characters.component.scss']
+  styleUrls: ['./characters.component.scss'],
+  encapsulation: ViewEncapsulation.None  // Use to disable CSS Encapsulation for this component
+
 })
 export class CharactersComponent implements OnInit {
 
